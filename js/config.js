754717118cce574967cc52d7e5fce8d52ab3e17b
mishshai <shailendra.mishra@moodys.com>
2020-02-19 20:17:03 +0000

    Updating region

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_LXCaC6rU7', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '27jp5j6rfjtefe43rukn6mr7de', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
